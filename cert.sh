982ca448f743949046e121ff96b164401d80acda
Mardhex <74248551+kmardhex@users.noreply.github.com>
2022-06-26 10:22:31 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -13,7 +13,6 @@ sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-wget autosc.me/acme.sh >/dev/null 2>&1
 bash acme.sh --install >/dev/null 2>&1
 bash acme.sh --register-account -m wapres.area82@gmail.com
 wget https://get.acme.sh >/dev/null 2>&1 | sh -s email=wapres.area82@gmail.com
