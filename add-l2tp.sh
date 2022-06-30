354aa8974a5a1d6d847b8c192454f4e310fadd68
santzx45 <106920145+santzx45@users.noreply.github.com>
2022-06-30 19:25:31 +0700

    Update add-l2tp.sh

--- add-l2tp.sh
+++ add-l2tp.sh
@@ -45,7 +45,7 @@ cat <<EOF
 L2TP/IPSEC PSK VPN
 
 Server IP    : $PUBLIC_IP
-IPsec PSK    : mdxvpn
+IPsec PSK    : sntzxvpn
 Username     : $VPN_USER
 Password     : $VPN_PASSWORD
 Expired ON   : $exp
