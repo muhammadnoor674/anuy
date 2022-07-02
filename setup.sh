9b0b00c03ea624a3a1e2d74eaa2d8e20af27967d
santzx45 <106920145+santzx45@users.noreply.github.com>
2022-07-02 07:45:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,21 +17,21 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/kmardhex/anu/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/kmardhex/anu/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/kmardhex/anu/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/kmardhex/anu/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/kmardhex/anu/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/kmardhex/anu/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/kmardhex/anu/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/kmardhex/anu/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 #install Set-br
-wget https://raw.githubusercontent.com/kmardhex/anu/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/santzx45/anu/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -57,7 +57,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/kmardhex/anu/main/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/santzx45/anu/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
@@ -65,7 +65,7 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-KANG MARDHEX-===========================" | tee -a log-install.txt
+echo "=================================-SantzX Vpn Store-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -105,12 +105,12 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : KANG MARDHEX"  | tee -a log-install.txt
-echo "   - Telegram                : t.me/mardhex"  | tee -a log-install.txt
-echo "   - Instagram               : @mardhex_zhe"  | tee -a log-install.txt
+echo "   - Dev/Main                : SantzX Vpn Store"  | tee -a log-install.txt
+echo "   - Telegram                : t.me/s4nX4z"  | tee -a log-install.txt
+echo "   - Instagram               : *"  | tee -a log-install.txt
 echo "   - Whatsapp                : *"  | tee -a log-install.txt
 echo "   - Facebook                : *" | tee -a log-install.txt
-echo "------------------Script Created By Kang Mardhex-----------------" | tee -a log-install.txt
+echo "------------------Script Created By SantzX Vpn Store-----------------" | tee -a log-install.txt
 echo ""
 echo " Server Reboot 5 Detik"
 sleep 5
