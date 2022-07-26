567e24f3c163e794a451ac168afd60a83ac50197
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2022-07-26 12:02:35 +0800

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -27,7 +27,7 @@ clear
 echo ""
 echo " Done Install Webmin"
 echo " $IP:10000"
-echo " AutoScriptVPS By  SantzX Vpn Store"
+echo " AutoScriptVPS By  Nyari Gratisan"
 }
 function restart () {
 echo " Restarting Webmin"
@@ -37,7 +37,7 @@ echo " Start Uninstall Webmin"
 clear
 echo ""
 echo " Done Restart Webmin"
-echo " AutoScriptVPS By SantzX Vpn Store"
+echo " AutoScriptVPS By Nyari Gratisan"
 }
 function uninstall () {
 echo " Removing Repositori Webmin"
@@ -50,7 +50,7 @@ apt autoremove --purge webmin -y > /dev/null 2>&1
 clear
 echo ""
 echo " Done Uninstall Webmin"
-echo " AutoScriptVPS By  SantzX Vpn Store"
+echo " AutoScriptVPS By  Nyari Gratisan"
 }
 if [[ "$cek" = "perl" ]]; then
 sts="${Info}"
@@ -65,7 +65,7 @@ echo -e " Status $sts"
 echo -e "  1. Install Webmin"
 echo -e "  2. Restart Webmin"
 echo -e "  3. Uninstall Webmin"
-echo -e " AutoScriptVPS By SantzX Vpn Store"
+echo -e " AutoScriptVPS By Nyari Gratisan"
 echo -e " Press CTRL+C to return"
 read -rp " Please Enter The Correct Number : " -e num
 if [[ "$num" = "1" ]]; then
