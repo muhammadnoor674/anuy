4e48a3d28646438146e9b048e6dfb5ea9d831585
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2022-07-26 13:36:34 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+Ini#!/bin/bash
 
 clear
 echo -e "=================================================" | lolcat
@@ -7,13 +7,14 @@ echo -e "#-----------------------------------------------#"
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By SantzX Vpn Store                           #"
+echo -e "# Build Up By Anuy                           #"
 echo -e "#-----------------------------------------------#"
 echo -e "# Thanks To                                     #"
 echo -e "#-----------------------------------------------#"
 echo -e "#                                      #"
 echo -e "# Allah                                 #"
 echo -e "# My Family                                     #"
-echo -e "# Madrex/MDX CLOUD                                        #"
+echo -e "#My Subscribers                                      #"
+echo -e "# Santz                                        #"
 echo -e "#-----------------------------------------------#"
 echo -e "================================================="
