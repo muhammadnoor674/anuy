d974eddc38fcbae0ac071f62546d14d9f10f2034
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2022-07-26 11:05:58 +0800

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/santzx45/anu/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install
@@ -26,12 +26,12 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/santzx45/anu/main/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/santzx45/anu/main/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/santzx45/anu/main/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/santzz45/anu/main/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/santzx45/anu/main/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/santzx45/anu/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
