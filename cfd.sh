e8cc0556c4feb05b090a005bd1ea73cda8dee3b5
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-23 03:01:52 +0800

    Update cfd.sh

--- cfd.sh
+++ cfd.sh
@@ -2,22 +2,25 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+ns_domain_cloudflare1() {
+DOMAIN=nyarigratisan.software
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+echo $sub > /root/cfku
+SUB_DOMAIN=${sub}.nyarigratisan.software
+CF_ID=anuybazoelk639@gmail.com
+CF_KEY=5fea2f8e2c45da4658778bcd81497c5dfdf33
+echo "nyarigratisan.software" > /root/domain
+echo $SUB_DOMAIN > /root/domain
 
-source /root/mail.conf
-DOMAIN=$domain
-CF_ID=$email
-CF_KEY=$key
 set -euo pipefail
-IP=$(wget -qO- ifconfig.me/ip);
-read -p "Masukan Subdomain Anda :" sub
-echo "Updating DNS for ${sub}..."
+IP=$(wget -qO- ipinfo.io/ip);
+echo "Record DNS ${SUB_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${sub}" \
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
@@ -27,12 +30,19 @@ if [[ "${#RECORD}" -le 10 ]]; then
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${sub}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "DONE...!"
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+cp /root/domain /etc/xray/domain
+
+echo -e "Done Record Domain For VPS"
+sleep 1
+}
+ns_domain_cloudflare1
