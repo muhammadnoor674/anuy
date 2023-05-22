a3c12c41fc1bdb3b54c9627fd8a80125acba75d3
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-23 03:34:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,21 +17,21 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/santzx45/anu/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/santzx45/anu/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/santzx45/anu/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/santzx45/anu/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/santzx45/anu/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/santzx45/anu/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/santzx45/anu/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/santzx45/anu/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 #install Set-br
-wget https://raw.githubusercontent.com/santzx45/anu/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/anuy/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -45,7 +45,7 @@ rm -f /root/cf.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://mdxcloud.net
+Documentation=https://wa.me/6285349326511
 
 [Service]
 Type=oneshot
@@ -57,7 +57,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/santzx45/anu/main/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/muhammadnoor674/anuy/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
