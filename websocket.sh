387b165c55475840c7761b7e58b85881c7a11516
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-23 02:23:34 +0800

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -9,14 +9,14 @@ echo Cek Hak Akses...
 sleep 0.5
 cd
 cd /usr/local/bin/
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/santzx45/anu/main/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/muhammadnoor674/anuy/main/ws-stunnel
 chmod +x /usr/local/bin/ws-stunnel
 
 # Installing Service
 cat > /etc/systemd/system/ws-stunnel.service << END
 [Unit]
-Description=Python Edu Proxy By SantzX
-Documentation=https://joysmart.xyz
+Description=Python Edu Proxy By NYARIGRATISAN
+Documentation=https://wa.me/6285349326511
 After=network.target nss-lookup.target
 
 [Service]
